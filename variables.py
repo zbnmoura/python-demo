55a753db0cc9259c91db712920a0d71f1d82fad1
bruno moura <zbnmoura@gmail.com>
2020-06-12 12:22:09 -0300

    variables demo

---
+++ variables.py
@@ -0,0 +1,30 @@
+# A variable is a container for a value, which can be of various types
+
+'''
+This is a 
+multiline comment
+or docstring (used to define a functions purpose)
+can be single or double quotes
+'''
+
+"""
+VARIABLE RULES:
+  - Variable names are case sensitive (name and NAME are different variables)
+  - Must start with a letter or an underscore
+  - Can have numbers but can not start with one
+"""
+x = 1 #int
+y = 2.3 #float
+name = 'brunonho' #string
+is_cool = False #bool
+
+# multiple assigment
+xx, yy, name2, is_cool2 = (1, 2.3, 'brunonhi', True)
+
+# basic math
+a = x + y
+
+# casting
+x = str(x)
+y = int(y)
+z = float(y)

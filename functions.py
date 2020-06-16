716fd21876defb175fa22dd15028f0b22fa71ba9
bruno moura <zbnmoura@gmail.com>
2020-06-16 11:17:48 -0300

    function demo

--- functions.py
+++ functions.py
@@ -1,7 +1,17 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
 # create function
+def sayHello(name):
+    print(f'hello {name}')
+
+# return values
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
 
 
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
+
+getsum2 = lambda num1, num2 : num1 + num2
+

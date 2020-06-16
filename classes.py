be4cbaf524ec1658c91e85e2934908f822b5f563
bruno moura <zbnmoura@gmail.com>
2020-06-16 14:33:00 -0300

    classes demo start

--- classes.py
+++ classes.py
@@ -1,2 +1,7 @@
 # A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
-
+#1:16:49
+#create class
+class User:
+    #constructor
+    def __init__(self, name):
+        self.name = name
